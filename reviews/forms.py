edcd6ca4de488cb0557a183517c87f779ca61694
JOO-10000-Zi <j4615545@gmail.com>
2022-10-07 15:53:36 +0900

    button change

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,3 +6,9 @@ class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = '__all__'
+        labels = {
+            'title': '리뷰 제목',
+            'content': '리뷰 내용',
+            'movie_name': '영화 이름',
+            'grade':'평점',
+        }
