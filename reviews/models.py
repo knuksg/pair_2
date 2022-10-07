038dce42e6d5733f8b399d841b1e4fa9c2709e5c
knuksg <ksgyo0121@gmail.com>
2022-10-07 09:48:45 +0900

    설정

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=30)
+    grade = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
