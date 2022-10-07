3cbaf297e95fc517caece790489c0622ba601262
knuksg <ksgyo0121@gmail.com>
2022-10-07 16:45:27 +0900

    avg

--- reviews/views.py
+++ reviews/views.py
@@ -1,26 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import Movie
 from .forms import MovieForm
+from django.db.models import Avg
 
 # Create your views here.
 def index(request):
     reviews = Movie.objects.order_by("-pk")
-    context = {"reviews": reviews}
+    grade = Movie.objects.aggregate(Avg("grade"))
+    context = {
+        "reviews": reviews,
+    }
     return render(request, "reviews/index.html", context)
-
-
-# def new(request):
-#     reviews = Movie.objects.get(pk=pk)
-#     if request.method == "POST":
-#         movie_form = MovieForm(request.POST, instance=reviews)
-#         if movie_form.is_valid():
-#             movie_form.save()
-#             return redirect("reviews:detail", reviews.pk)
-#     else:
-#         movie_form = MovieForm(instance=reviews)
-#     context = {"movie_form": movie_form}
-
-#     return render(request, "reviews/update.html", context)
 
 
 def create(request):
@@ -33,18 +23,6 @@ def create(request):
         movie_form = MovieForm()
     context = {"movie_form": movie_form}
     return render(request, "reviews/new.html", context=context)
-
-    # title = request.POST.get("title")
-    # content = request.POST.get("content")
-    # movie_name = request.POST.get("movie_name")
-    # grade = request.POST.get("grade")
-
-    # Movie.objects.create(
-    #     title=title, content=content, movie_name=movie_name, grade=grade
-    # )
-
-    # # create.html을 별도로 생성하지 않고 글쓰기가 완료되면 index 페이지로 넘어가도록 redirect
-    # return redirect("reviews:index")
 
 
 def detail(request, pk):
