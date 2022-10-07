188085386f8f2959cb18690d7e8c94d25ac91ae9
hjkim1350 <hjkim1350@daum.net>
2022-10-07 10:30:20 +0900

    마이그레이션 완료, new/create 기능 구현

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,4 +6,7 @@ app_name = "reviews"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("new/", views.new, name="new"),
+    path("create/", views.create, name="create"),
+    path("detail/<int:pk>", views.detail, name="detail"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return render(request, "reviews/index.html")
+    reviews = Movie.objects.order_by("-pk")
+    context = {"reviews": reviews}
+    return render(request, "reviews/index.html", context)
+
+
+def new(request):
+    return render(request, "reviews/new.html")
+
+
+def create(request):
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    movie_name = request.POST.get("movie_name")
+    grade = request.POST.get("grade")
+
+    Movie.objects.create(
+        title=title, content=content, movie_name=movie_name, grade=grade
+    )
+
+    # create.html을 별도로 생성하지 않고 글쓰기가 완료되면 index 페이지로 넘어가도록 redirect
+    return redirect("reviews:index")
+
+
+def detail(request, pk):
+    reviews = Movie.objects.get(pk=pk)
+    context = {"reviews": reviews}
+
+    return render(request, "reviews/detail.html", context)
