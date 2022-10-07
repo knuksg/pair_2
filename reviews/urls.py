ada3948c29f307aec392d6d56266e9962832f874
JOO-10000-Zi <j4615545@gmail.com>
2022-10-07 11:06:15 +0900

    add update/delete

--- reviews/urls.py
+++ reviews/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("new/", views.new, name="new"),
     path("create/", views.create, name="create"),
     path("detail/<int:pk>", views.detail, name="detail"),
+    path("update/<int:pk>", views.update, name="update"),
+    path("delete/<int:pk>", views.delete, name="delete"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Movie
+from .forms import MovieForm
 
 # Create your views here.
 def index(request):
@@ -31,3 +32,23 @@ def detail(request, pk):
     context = {"reviews": reviews}
 
     return render(request, "reviews/detail.html", context)
+
+def update(request, pk):
+    reviews = Movie.objects.get(pk=pk)
+    if request.method == "POST":
+        movie_form = MovieForm(request.POST, instance=reviews)
+        if movie_form.is_valid():
+            movie_form.save()
+            return redirect("reviews:detail", reviews.pk)
+    else:
+        movie_form = MovieForm(instance=reviews)
+    context = {
+        'movie_form': movie_form
+    }
+    return render(request, "reviews/update.html", context)
+
+def delete(request, pk):
+   reviews = Movie.objects.get(pk=pk)
+   reviews.delete()
+
+   return redirect("reviews:index")
